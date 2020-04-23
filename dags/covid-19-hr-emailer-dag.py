b8e58d57e8fd84a8ab2b1b2ee4b1a3459da62ff3
Gordon Inggs <gordon.inggs@gmail.com>
2020-04-23 11:43:14 +0200

    Adding HR Business Partner emailer dag

---
+++ dags/covid-19-hr-emailer-dag.py
@@ -0,0 +1,83 @@
+from airflow import DAG
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
+from airflow.contrib.kubernetes.secret import Secret
+
+from datetime import datetime, timedelta
+
+DAG_STARTDATE = datetime(2020, 4, 23)
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': DAG_STARTDATE,
+    'email': ['gordon.inggs@capetown.gov.za'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+}
+
+startup_cmd = (
+    "mkdir $COVID_19_WIDGETS_DIR && "
+    "curl $COVID_19_DEPLOY_URL/$COVID_19_DEPLOY_FILE -o $COVID_19_WIDGETS_DIR/$COVID_19_DEPLOY_FILE && "
+    "cd $COVID_19_WIDGETS_DIR && unzip $COVID_19_DEPLOY_FILE && "
+    "pip3 install $DB_UTILS_LOCATION/$DB_UTILS_PKG"
+)
+
+# At 10pm, every weekday
+dag_interval = "0 18 * * 1-5"
+dag = DAG('covid-19-hr-emailer',
+          start_date=DAG_STARTDATE,
+          catchup=False,
+          default_args=default_args,
+          schedule_interval=dag_interval,
+          concurrency=1)
+
+# env variables for inside the k8s pod
+k8s_run_env = {
+    'SECRETS_PATH': '/secrets/secrets.json',
+    'COVID_19_DEPLOY_FILE': 'covid-19-widgets.zip',
+    'COVID_19_DEPLOY_URL': 'https://ds2.capetown.gov.za/covid-19-widgets-deploy',
+    'COVID_19_WIDGETS_DIR': '/covid-19-widgets',
+    'DB_UTILS_LOCATION': 'https://ds2.capetown.gov.za/db-utils',
+    'DB_UTILS_PKG': 'db_utils-0.3.2-py2.py3-none-any.whl'
+}
+
+# airflow-workers' secrets
+secret_file = Secret('volume', '/secrets', 'airflow-workers-secret')
+
+# arguments for the k8s operator
+k8s_run_args = {
+    "image": "cityofcapetown/datascience:python",
+    "namespace": 'airflow-workers',
+    "is_delete_operator_pod": True,
+    "get_logs": True,
+    "in_cluster": True,
+    "secrets": [secret_file],
+    "env_vars": k8s_run_env,
+    "image_pull_policy": "Always",
+     "startup_timeout_seconds": 60*30,
+}
+
+
+def covid_19_widget_task(task_name, task_kwargs={}):
+    """Factory for k8sPodOperator"""
+    name = "covid-19-hr-emailer-{}".format(task_name)
+    run_args = {**k8s_run_args.copy(), **task_kwargs}
+    run_cmd = "bash -c '{} && \"$COVID_19_WIDGETS_DIR\"/bin/{}.sh'".format(startup_cmd, task_name)
+
+    operator = KubernetesPodOperator(
+        cmds=["bash", "-cx"],
+        arguments=[run_cmd],
+        name=name,
+        task_id=name,
+        dag=dag,
+        execution_timeout=timedelta(hours=1),
+        **run_args
+    )
+
+    return operator
+
+
+# Defining tasks
+EMAIL_TASK = 'hr-bp-emailer'
+email_operator = covid_19_widget_task(EMAIL_TASK)
