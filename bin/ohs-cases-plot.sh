4ac001f12d1191d0d23b3d6be33aa69a20abe111
Delyno <delyno.dutoit@capetown.gov.za>
2020-06-08 14:43:44 +0200

    ohs plot shell

--- bin/ohs-cases-plot.sh
+++ bin/ohs-cases-plot.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-python3 ./ohs_data_to_minio.py
+DIRECTORATE_PREFIX=$1
+DIRECTORATE_TITLE=$2
+
+python3 ./ohs_cases_plot_to_minio.py "$DIRECTORATE_PREFIX" "$DIRECTORATE_TITLE"
