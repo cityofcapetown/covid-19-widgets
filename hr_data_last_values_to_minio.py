c4723abfca0266261a708880cb16c7d72b2eabbb
Gordon Inggs <gordon.inggs@gmail.com>
2020-06-09 07:23:23 +0200

    Changing over to new HR statuses.

--- hr_data_last_values_to_minio.py
+++ hr_data_last_values_to_minio.py
@@ -29,31 +29,26 @@ TZ_STRING = "Africa/Johannesburg"
 WORKING_STATUS = "working"
 NOT_WORKING_STATUS = "not-working"
 STATUSES_TO_SUCCINCT_MAP = {
-    "Working remotely (NO COVID 19 exposure)": WORKING_STATUS,
-    "At work (on site)": WORKING_STATUS,
-    "On leave": NOT_WORKING_STATUS,
-    "On suspension": NOT_WORKING_STATUS,
-    "Absent from work (unauthorised)": NOT_WORKING_STATUS,
-    "Quarantine leave – working remotely": WORKING_STATUS,
-    "Quarantine leave – unable to work remotely": NOT_WORKING_STATUS,
-    "Quarantine leave – working remotely, COVID 19 exposure / isolation": WORKING_STATUS,
-    "Sick (linked to COVID 19)": NOT_WORKING_STATUS,
-    "Sick (NOT linked to COVID 19)": NOT_WORKING_STATUS,
-    "On Lockdown leave – unable to work remotely": NOT_WORKING_STATUS,
-    "On Lockdown leave – able to work remotely": NOT_WORKING_STATUS
+    'At work': WORKING_STATUS,
+    'Working from Home': WORKING_STATUS,
+    'Off-Site': WORKING_STATUS,
+    'COVID-19 Quarantine – Working': WORKING_STATUS,
+    'COVID-19 Quarantine - Not Working': NOT_WORKING_STATUS,
+    'COVID-19 Lockdown': NOT_WORKING_STATUS,
+    'Sick Leave (linked to COVID-19)': NOT_WORKING_STATUS,
+    'Sick Leave (NOT linked to COVID-19)': NOT_WORKING_STATUS,
+    'Leave': NOT_WORKING_STATUS,
+    'Suspended': NOT_WORKING_STATUS,
+    'Absent': NOT_WORKING_STATUS
 }
 REMOTE_WORK_STATUSES = {
-    "Working remotely (NO COVID 19 exposure)",
-    "Quarantine leave – working remotely",
-    "Quarantine leave – working remotely, COVID 19 exposure / isolation"
+    'Working from Home', 'Off-Site', 'COVID-19 Quarantine – Working'
 }
 SICK_STATUSES = {
-    "Sick (linked to COVID 19)",
-    "Sick (NOT linked to COVID 19)"
+    'Sick Leave (linked to COVID-19)', 'Sick Leave (NOT linked to COVID-19)'
 }
 COVID_STATUSES = {
-    "Sick (linked to COVID 19)",
-    "Quarantine leave – working remotely, COVID 19 exposure / isolation"
+    'Sick Leave (linked to COVID-19)', 'COVID-19 Quarantine – Working', 'COVID-19 Quarantine - Not Working'
 }
 
 WIDGETS_RESTRICTED_PREFIX = "widgets/private/business_continuity_"
@@ -116,7 +111,7 @@ def get_current_hr_df(hr_df):
     # then, sort, drop duplicates to only keep the most recent one
     current_hr_df = hr_df[
         hr_df[DATE_COL_NAME].dt.date >= date_window_start
-    ].sort_values(
+        ].sort_values(
         by=[STAFF_NUMBER_COL_NAME, DATE_COL_NAME], ascending=False
     ).drop_duplicates(
         subset=[STAFF_NUMBER_COL_NAME]
