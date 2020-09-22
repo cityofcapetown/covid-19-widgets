e1c87dc2cdad91d8bc956d12e7f7b52deba8739f
Gordon Inggs <gordon.inggs@gmail.com>
2020-09-22 15:52:40 +0200

    Adding missing data to service delivery map layer loading

--- service_delivery_map_layers_to_minio.py
+++ service_delivery_map_layers_to_minio.py
@@ -39,7 +39,8 @@ LAYER_FILES = (
     (f"city_all_{epi_map_case_layers_to_minio.DISTRICT_COUNT_SUFFIX}", MINIO_COVID_BUCKET, city_map_layers_to_minio.CASE_MAP_PREFIX),
     (f"city_all_{epi_map_case_layers_to_minio.HEX_L8_COUNT_SUFFIX}".replace(".geojson", ".json"), MINIO_COVID_BUCKET, city_map_layers_to_minio.CASE_MAP_PREFIX),
     (f"city_all_{epi_map_case_layers_to_minio.DISTRICT_COUNT_SUFFIX}".replace(".geojson", ".json"), MINIO_COVID_BUCKET, city_map_layers_to_minio.CASE_MAP_PREFIX),
-    ("city-absence-counts-hex.geojson", MINIO_COVID_BUCKET, DATA_RESTRICTED_PREFIX)
+    ("city-absence-counts-hex.geojson", MINIO_COVID_BUCKET, DATA_RESTRICTED_PREFIX),
+    ("sl_du_pop_est_2019_hex9.geojson", MINIO_COVID_BUCKET, DATA_VULNERABILITY_PREFIX),
 )
 
 if __name__ == "__main__":
